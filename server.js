24420c5a833999b572505f66303e40d658df14a5
Dominique Sakrisson <domsakrisson@gmailcom>
2021-09-19 12:00:05 -0700

    Add middleware

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const app = require('./lib/app')
 const PORT = process.env.PORT || 3000; 
 
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "http://<YOUR-APP-NAME>.herokuapp.com"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  
 app.listen(PORT, () => {
     console.log(`Started on ${PORT}`);
 });
