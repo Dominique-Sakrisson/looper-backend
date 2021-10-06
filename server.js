f2f822d8f6d1dc8da70be94daf8eb8829491809d
Dominique Sakrisson <domsakrisson@gmailcom>
2021-10-06 16:29:43 -0700

    Fix routing issue

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const app = require('./lib/app')
 const PORT = process.env.PORT || 3000; 
 
-app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      // "http://localhost:7891"
-      // "http://<YOUR-APP-NAME>.herokuapp.com"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   
 app.listen(PORT, () => {
     console.log(`Started on ${PORT}`);
